4d5903f93645dfce46ab94ac60056dc17ac8d4d7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-10 10:17:10 +0700

    get profile me

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
-    Route::post('/createuser', 'createuser');
+});
+
+Route::controller(AuthController::class)->middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', 'logout');
+    Route::get('/me', 'me');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
