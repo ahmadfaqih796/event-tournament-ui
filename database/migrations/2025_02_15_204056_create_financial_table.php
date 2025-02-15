f31437247d4abcb059e14dac4b9b332877772213
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-16 03:55:18 +0700

    financial migration

---
+++ database/migrations/2025_02_15_204056_create_financial_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tr_id')->constrained('tournament_registrations')->onDelete('cascade');
+            $table->string('event_name');
+            $table->string('tournament_name');
+            $table->string('game_name');
+            $table->string('price');
+            $table->string('pay');
+            $table->string('paid_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial');
+    }
+};
