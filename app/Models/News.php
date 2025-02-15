a6ad42082b8b9d6b0e281ff6b53696386e6f8251
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-15 17:05:15 +0700

    news model

---
+++ app/Models/News.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'category',
+        'image',
+        'created_by'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
