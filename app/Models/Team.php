5e571a897f8ed23e6e2d105ec025b500fd37edae
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-15 16:47:06 +0700

    team model

---
+++ app/Models/Team.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'leader_id'
+    ];
+
+    public function leader()
+    {
+        return $this->belongsTo(User::class, 'leader_id');
+    }
+
+    // public function members()
+    // {
+    //     return $this->hasMany(TeamMember::class);
+    // }
+}
