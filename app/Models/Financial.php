33ce3017dbb64957c5a519381ab8a1b9121ea912
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-16 03:58:30 +0700

    financial model

--- app/Models/Financial.php
+++ app/Models/Financial.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Financial extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tr_id',
+        'event_name',
+        'tournament_name',
+        'game_name',
+        'price',
+        'pay',
+        'paid_by'
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(TournamentRegistration::class, 'tr_id');
+    }
 }
