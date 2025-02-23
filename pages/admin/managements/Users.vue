def41c9efaec44803cc3389f21f4b699ad1d2bb8
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-23 23:47:32 +0700

    snackbar

--- pages/admin/managements/Users.vue
+++ pages/admin/managements/Users.vue
@@ -4,12 +4,14 @@ import UiParentCard from "@/components/shared/UiParentCard.vue";
 import TablePagination from "@/components/style-components/table/TablePagination.vue";
 import { computed, onMounted, ref, watch } from "vue";
 import { useUserService } from "~/services/userService";
+import { useSnackbar } from "~/composables/useSnackbar";
 
 const { fetchUsers, addUser, updateUser, deleteUser } = useUserService();
+const { showSnackbar } = useSnackbar();
 
 const isModalOpen = ref(false);
 const isLoading = ref(false);
-const form = ref({ id: null, name: "" });
+const form = ref({ id: null, name: "", username: "", password: "", role: "peserta" });
 const items = ref([]);
 const modalType = ref<"add" | "edit" | "delete" | null>(null);
 const reactiveItems = computed(() => items.value);
@@ -17,7 +19,7 @@ const reactiveItems = computed(() => items.value);
 const openModal = (type: "add" | "edit" | "delete", rowData?: any) => {
   modalType.value = type;
   if (type === "add") {
-    form.value = { id: null, name: "" };
+    form.value = { id: null, name: "", username: "", password: "", role: "peserta" };
   } else if (rowData) {
     form.value = { ...rowData };
   }
@@ -29,15 +31,22 @@ const confirmAction = async () => {
   try {
     if (modalType.value === "add") {
       await addUser(form.value);
+      showSnackbar({ message: "User berhasil ditambahkan!", color: "success" });
     } else if (modalType.value === "edit" && form.value.id !== null) {
       await updateUser(form.value.id, form.value);
+      showSnackbar({ message: "User berhasil diubah!", color: "success" });
     } else if (modalType.value === "delete" && form.value.id !== null) {
       await deleteUser(form.value.id);
+      showSnackbar({ message: "User berhasil dihapus!", color: "success" });
     }
     isModalOpen.value = false;
     items.value = await fetchUsers();
   } catch (error) {
     console.error("Action failed:", error);
+    if (error) {
+      const message = error.response.data.message;
+      showSnackbar({ message: message, color: "error" });
+    }
   } finally {
     isLoading.value = false;
   }
@@ -79,6 +88,9 @@ watch(items, (newItems) => {
   <BaseModal v-model="isModalOpen" :loading="isLoading" :title="modalType === 'add' ? 'Tambah Data' : modalType === 'edit' ? 'Edit Data' : 'Hapus Data'" @confirm="confirmAction">
     <template v-if="modalType !== 'delete'">
       <v-text-field v-model="form.name" label="Name"></v-text-field>
+      <v-text-field v-model="form.username" label="Username"></v-text-field>
+      <v-text-field v-model="form.password" label="Password"></v-text-field>
+      <v-select v-model="form.role" :items="['peserta', 'komunitas']" label="Role"></v-select>
     </template>
     <template v-else>
       <p>Apakah Anda yakin ingin menghapus data ini?</p>
